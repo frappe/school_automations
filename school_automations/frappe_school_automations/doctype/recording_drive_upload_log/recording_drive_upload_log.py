0ff24c6abf0dd9950156eed5e1f6b2813047e85d
Hussain Nagaria <hussainbhaitech@gmail.com>
2025-03-07 18:03:09 +0530

    feat: track link in Live Class and announce to students

--- school_automations/frappe_school_automations/doctype/recording_drive_upload_log/recording_drive_upload_log.py
+++ school_automations/frappe_school_automations/doctype/recording_drive_upload_log/recording_drive_upload_log.py
@@ -1,9 +1,53 @@
 # Copyright (c) 2025, Frappe and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class RecordingDriveUploadLog(Document):
-	pass
+	def on_submit(self):
+		frappe.db.set_value("LMS Live Class", self.live_class, {
+			"custom_recording_uploaded": True,
+			"custom_recording_url": self.drive_link
+		})
+
+		try:
+			make_announcement_to_batch_students(self.live_class, self.drive_link)
+		except Exception:
+			frappe.log_error("Unable to make batch announcement for Recording!")
+
+
+def make_announcement_to_batch_students(class_id: str, recording_link: str):
+	from frappe.core.doctype.communication.email import make
+
+	live_class_doc = frappe.get_doc('LMS Live Class', class_id)
+	batch_name = live_class_doc.batch_name
+	students = frappe.db.get_all('LMS Batch Enrollment', filters={'batch': batch_name}, pluck='member')
+
+	instructor_email = frappe.db.get_all(
+		'Course Instructor',
+		filters={'parent': batch_name, 'parenttype': 'LMS Batch'},
+		fields=['instructor.email as email'],
+		pluck='email',
+		limit=1,
+	)[0]
+
+	content = f"""Hi!
+
+Recording for **{live_class_doc.title}** is now available on this [link]({recording_link}).
+
+
+Regards,
+Team Frappe School
+"""
+
+	make(
+		'LMS Batch',
+		batch_name,
+		subject='Live class recording now available!',
+		cc=instructor_email,
+		send_email=1,
+		recipients=students,
+		content=frappe.utils.md_to_html(content),
+	)
--- school_automations/public/js/lms_live_class_extend.js
+++ school_automations/public/js/lms_live_class_extend.js
@@ -1,15 +1,19 @@
 frappe.ui.form.on('LMS Live Class', {
 	refresh(frm) {
-		const button = frm.add_custom_button("Upload Recording to Google Drive", () => {
-			frappe.call({
-				method: "school_automations.utils.queue_recording_download",
-				args: {
-					class_id: frm.doc.name
-				},
-				btn: button
-			}).then(() => {
-				frappe.show_alert("Meeting will be uploaded soon! Check Drive Upload Log for more.")
+		const is_already_uploaded = frm.doc.custom_recording_uploaded;
+
+		if (!is_already_uploaded) {
+			const button = frm.add_custom_button("Upload Recording to Google Drive", () => {
+				frappe.call({
+					method: "school_automations.utils.queue_recording_download",
+					args: {
+						class_id: frm.doc.name
+					},
+					btn: button
+				}).then(() => {
+					frappe.show_alert("Meeting will be uploaded soon! Check Drive Upload Log for more.")
+				})
 			})
-		})
+		}
 	}
 })
